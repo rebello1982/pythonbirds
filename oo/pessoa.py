fac7bdde62ceb39328ef2c7a5b199af1d5eeef02
rebello1982 <ludcas@gmail.com>
2020-05-20 12:31:54 -0300

    Criado e removido atributo dinâmico de objeto do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -8,15 +8,17 @@ class Pessoa:
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    rebello = Pessoa(nome='Rebello')
-    juliana = Pessoa(rebello, nome='Juliana')
-    print(Pessoa.cumprimentar(juliana))
-    print(id(juliana))
-    print(juliana.cumprimentar())
-    print(juliana.nome)
-    print(juliana.idade)
-    for filho in juliana.filhos:
+    lucas = Pessoa(nome='Lucas')
+    diogo = Pessoa(lucas, nome='Diogo')
+    print(Pessoa.cumprimentar(diogo))
+    print(id(diogo))
+    print(diogo.cumprimentar())
+    print(diogo.nome)
+    print(diogo.idade)
+    for filho in diogo.filhos:
         print(filho.nome)
-
-
+    diogo.sobrenome= 'Rebello'
+    del diogo.filhos
+    print(diogo.__dict__)
+    print(lucas.__dict__)
 
